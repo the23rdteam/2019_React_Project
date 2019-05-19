db2b5aee8dad392f96f33f547ab1338b369b4b70
IMHOJEONG <39ghwjd@naver.com>
2019-05-20 07:36:33 +0900

    첫 번째 실습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// node_modules에서 가져온 파일은 ./ 없이 그냥 쓰면 됌
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
